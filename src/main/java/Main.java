9af3a956fff943c62296cd8b93a5df9fe893273e
Mohamed Amine Oueslati <amine.oueslati@aderivatives.com>
2024-07-26 11:41:36 +0100

    pass stage

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,4 @@
-import java.io.IOException;
+import java.io.*;
 import java.net.ServerSocket;
 
 public class Main {
@@ -6,16 +6,22 @@ public class Main {
 
     System.out.println("Logs from your program will appear here!");
 
-     try {
-       ServerSocket serverSocket = new ServerSocket(4221);
-
+     try(ServerSocket serverSocket = new ServerSocket(4221)) {
        // Since the tester restarts your program quite often, setting SO_REUSEADDR
        // ensures that we don't run into 'Address already in use' errors
        serverSocket.setReuseAddress(true);
 
        var clientSocket = serverSocket.accept(); // Wait for connection from client.
        System.out.println("accepted new connection");
-       clientSocket.getOutputStream().write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+       var input = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+       var request = input.readLine().split(" ");
+       var method = request[0];
+       var url = request[1];
+       if (method.equals("GET") && (url.equals("/") || url.equals("/index.html"))) {
+         clientSocket.getOutputStream().write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+       } else {
+         clientSocket.getOutputStream().write("HTTP/1.1 404 Not Found\r\n\r\n".getBytes());
+       }
      } catch (IOException e) {
        System.out.println("IOException: " + e.getMessage());
      }
