a529c86524fa0c4b105d92d196b103ea41e11a6d
Mohamed Amine Oueslati <amine.oueslati@aderivatives.com>
2024-07-31 14:29:17 +0100

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -53,6 +53,7 @@ public class Main {
   private static RequestInput parseRequest(Socket socket) throws IOException {
     var input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 
+    // Read request line
     var requestLine = input.readLine();
     if (StringUtils.isBlank(requestLine))
       return null;
@@ -60,6 +61,7 @@ public class Main {
     var method = requestLineArr[0];
     var url = requestLineArr[1];
 
+    // Read headers
     String headerLine = input.readLine();
     var headers = new HashMap<String, String>();
     while (StringUtils.isNotBlank(headerLine)) {
@@ -68,6 +70,7 @@ public class Main {
       headerLine = input.readLine();
     }
 
+    // Read request body
     var body = new StringBuilder();
     while (input.ready()) {
       body = body.append((char) input.read());
@@ -77,55 +80,76 @@ public class Main {
   }
 
   private static String processRequest(RequestInput request, String directory) throws IOException {
+    var response = "";
+
     // Process simple request
     if (request.method().equals("GET") && (request.url().equals("/") || request.url().equals("/index.html")))
-      return "HTTP/1.1 200 OK\r\n\r\n";
+      response = "HTTP/1.1 200 OK\r\n\r\n";
 
     // Read request paramater and write it back to the body
     if (request.method().equals("GET") && (request.url().startsWith("/echo/"))) {
       var str = request.url().split("/");
-      if (str.length >= 3) {
-        return "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " + str[2].length() + "\r\n\r\n"
-            + str[2];
+      if (str.length < 3) {
+        response = "HTTP/1.1 400 Bad Request\r\n\r\n";
       } else {
-        return "HTTP/1.1 404 Not Found\r\n\r\n";
+        response = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " + str[2].length() + "\r\n\r\n"
+            + str[2];
       }
     }
 
     // Read header and write it back to the body
     if (request.method().equals("GET") && request.url().equals("/user-agent")) {
       var header = request.headers().get("User-Agent");
-      return "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " + header.length() + "\r\n\r\n" + header;
+      response = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " + header.length() + "\r\n\r\n"
+          + header;
     }
 
     // Return a file
     if (request.method().equals("GET") && request.url().startsWith("/files")) {
       var url = request.url().split("/");
-      if (url.length < 3)
-        return "HTTP/1.1 404 Not Found\r\n\r\n";
-      var fileName = url[2];
-
-      var file = new File(directory + fileName);
-      if (!file.exists()) return "HTTP/1.1 404 Not Found\r\n\r\n";
-      var data = Files.readAllLines(file.toPath()).stream().collect(Collectors.joining("\n"));
-      return "HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: " + data.length() + "\r\n\r\n" + data;
+      if (url.length < 3) {
+        response = "HTTP/1.1 400 Bad Request\r\n\r\n";
+      } else {
+        var fileName = url[2];
+        var file = new File(directory + fileName);
+        if (!file.exists()) {
+          response = "HTTP/1.1 404 Not Found\r\n\r\n";
+        } else {
+          var data = Files.readAllLines(file.toPath()).stream().collect(Collectors.joining("\n"));
+          response = "HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: " + data.length()
+              + "\r\n\r\n" + data;
+        }
+      }
     }
 
     // Create file
     if (request.method().equals("POST") && request.url().startsWith("/files")) {
       var url = request.url().split("/");
-      if (url.length < 3)
-        return "HTTP/1.1 404 Not Found\r\n\r\n";
-      var fileName = url[2];
-      var file = new File(directory + fileName);
-      if (!file.createNewFile()) return "HTTP/1.1 409 File already exists\r\n\r\n";
-      var fileWriter = new FileWriter(file);
-      fileWriter.write(request.body());
-      fileWriter.close();
-      return "HTTP/1.1 201 Created\r\n\r\n";
+      if (url.length < 3) {
+        response = "HTTP/1.1 400 Bad Request\r\n\r\n";
+      } else {
+        var fileName = url[2];
+        var file = new File(directory + fileName);
+        if (!file.createNewFile()) {
+          response = "HTTP/1.1 409 Conflict\r\n\r\n";
+        } else {
+          var fileWriter = new FileWriter(file);
+          fileWriter.write(request.body());
+          fileWriter.close();
+          response = "HTTP/1.1 201 Created\r\n\r\n";
+        }
+      }
     }
 
-    return "HTTP/1.1 404 Not Found\r\n\r\n";
+    // Compression
+    if (request.headers().containsKey("Accept-Encoding") && request.headers().get("Accept-Encoding").equals("gzip")) {
+      var idx = response.indexOf("\r\n") + 2;
+      response = response.substring(0, idx).concat("Content-Encoding: gzip\r\n").concat(response.substring(idx));
+    }
+
+    if (StringUtils.isEmpty(response))
+      response = "HTTP/1.1 404 Not Found\r\n\r\n";
+    return response;
   }
 
   private static void writeResponse(Socket socket, String response) throws IOException {
