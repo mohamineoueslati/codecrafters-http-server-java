2e2d57008e67cf223867ddefcc4c20fb116bfed1
Mohamed Amine Oueslati <amine.oueslati@aderivatives.com>
2024-07-31 10:33:17 +0100

    pass stage

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,8 +7,6 @@ import org.apache.commons.lang3.StringUtils;
 
 public class Main {
 
-  private static Socket clientSocket;
-
   public static void main(String[] args) {
 
     System.out.println("Logs from your program will appear here!");
@@ -16,16 +14,27 @@ public class Main {
     try (ServerSocket serverSocket = new ServerSocket(4221)) {
       // Since the tester restarts your program quite often, setting SO_REUSEADDR
       // ensures that we don't run into 'Address already in use' errors
-
       serverSocket.setReuseAddress(true);
-      clientSocket = serverSocket.accept(); // Wait for connection from client.
-      System.out.println("accepted new connection");
 
-      var request = parseRequest();
-      if (request != null) {
-        writeResponse(processRequest(request));
-      } else {
-        writeResponse("HTTP/1.1 404 Not Found\r\n\r\n");
+      while (true) {
+        var clientSocket = serverSocket.accept(); // Wait for connection from client.
+        System.out.println("accepted new connection");
+
+        Runnable process = () -> {
+          try {
+            var request = parseRequest(clientSocket);
+            if (request != null) {
+              writeResponse(clientSocket, processRequest(request));
+            } else {
+              writeResponse(clientSocket, "HTTP/1.1 404 Not Found\r\n\r\n");
+            }
+          } catch (IOException e) {
+            System.out.println("IOException: " + e.getMessage());
+          }
+        };
+
+        var thread = new Thread(process);
+        thread.start();
       }
 
     } catch (IOException e) {
@@ -33,11 +42,12 @@ public class Main {
     }
   }
 
-  private static RequestInput parseRequest() throws IOException {
-    var input = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+  private static RequestInput parseRequest(Socket socket) throws IOException {
+    var input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 
     var requestLine = input.readLine();
-    if (StringUtils.isBlank(requestLine)) return null;
+    if (StringUtils.isBlank(requestLine))
+      return null;
     var requestLineArr = requestLine.split(" ");
     var method = requestLineArr[0];
     var url = requestLineArr[1];
@@ -46,7 +56,7 @@ public class Main {
     var headers = new HashMap<String, String>();
     while (StringUtils.isNotBlank(headerLine)) {
       var headerLineArr = headerLine.split(" ");
-      headers.put(headerLineArr[0].substring(0, headerLineArr[0].length()-1), headerLineArr[1]);
+      headers.put(headerLineArr[0].substring(0, headerLineArr[0].length() - 1), headerLineArr[1]);
       headerLine = input.readLine();
     }
 
@@ -67,13 +77,12 @@ public class Main {
     } else if (request.method().equals("GET") && request.url().equals("/user-agent")) {
       var header = request.headers().get("User-Agent");
       return "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " + header.length() + "\r\n\r\n" + header;
-    }
-     else {
+    } else {
       return "HTTP/1.1 404 Not Found\r\n\r\n";
     }
   }
 
-  private static void writeResponse(String response) throws IOException {
-    clientSocket.getOutputStream().write(response.getBytes());
+  private static void writeResponse(Socket socket, String response) throws IOException {
+    socket.getOutputStream().write(response.getBytes());
   }
 }
